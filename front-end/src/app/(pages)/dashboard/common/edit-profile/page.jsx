f1f04275d74d6ae87c9ac46591da2e4885d7d885
Yasirhassan1 <yasuhassan7896@gmail.com>
2025-12-15 23:01:22 +0500

    feat: Implement an "Edit Profile" page using a new reusable `Field` component.

--- front-end/src/app/(pages)/dashboard/common/edit-profile/page.jsx
+++ front-end/src/app/(pages)/dashboard/common/edit-profile/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Field from "@/app/components/profile/Field";
 import {data} from "./data";
 export default function EditProfile(){
@@ -5,21 +6,21 @@ export default function EditProfile(){
         <div className="container flex flex-col gap-4">
         <h3 className="">Edit Profile</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-12">
-       <Field lable="Name" value={data.name}/>
-       <Field lable="Application No" value={data.applicationNo}/>
-       <Field lable="Registration No" value={data.registrationNo}/>
-       <Field lable="Program" value={data.program}/>
-       <Field lable="Department" value={data.department}/>
-       <Field lable="Session" value={data.session}/>
-       <Field lable="Gender" value={data.gender}/>
-       <Field lable="Date of Birth" value={data.dateOfBirth}/>
-       <Field lable="Village" value={data.village}/>
-       <Field lable="Permanent Address" value={data.permanentAddress}/>
-       <Field lable="Cell No" value={data.cellNo}/>
-       <Field lable="Email" value={data.email}/>
-       <Field lable="Father Name" value={data.fatherName}/>
-       <Field lable="Father Cell No" value={data.fatherCellNo}/>
-       <Field lable="Father Occupation" value={data.fatherOccupation}/>
+       <Field lable="Name" value={data.name} onChange={(e)=>data.name=e.target.value}/>
+       <Field lable="Application No" value={data.applicationNo} onChange={(e)=>data.applicationNo=e.target.value}/>
+       <Field lable="Registration No" value={data.registrationNo} onChange={(e)=>data.registrationNo=e.target.value}/>
+       <Field lable="Program" value={data.program} onChange={(e)=>data.program=e.target.value}/>
+       <Field lable="Department" value={data.department} onChange={(e)=>data.department=e.target.value}/>
+       <Field lable="Session" value={data.session} onChange={(e)=>data.session=e.target.value}/>
+       <Field lable="Gender" value={data.gender} onChange={(e)=>data.gender=e.target.value}/>
+       <Field lable="Date of Birth" value={data.dateOfBirth} onChange={(e)=>data.dateOfBirth=e.target.value}/>
+       <Field lable="Village" value={data.village} onChange={(e)=>data.village=e.target.value}/>
+       <Field lable="Permanent Address" value={data.permanentAddress} onChange={(e)=>data.permanentAddress=e.target.value}/>
+       <Field lable="Cell No" value={data.cellNo} onChange={(e)=>data.cellNo=e.target.value}/>
+       <Field lable="Email" value={data.email} onChange={(e)=>data.email=e.target.value}/>
+       <Field lable="Father Name" value={data.fatherName} onChange={(e)=>data.fatherName=e.target.value}/>
+       <Field lable="Father Cell No" value={data.fatherCellNo} onChange={(e)=>data.fatherCellNo=e.target.value}/>
+       <Field lable="Father Occupation" value={data.fatherOccupation} onChange={(e)=>data.fatherOccupation=e.target.value}/>
         </div>
 
         <div className="buttons flex flex-wrap gap-4 mt-12">
--- front-end/src/app/components/profile/Field.jsx
+++ front-end/src/app/components/profile/Field.jsx
@@ -1,8 +1,8 @@
-export default function Field({lable, value}){
+export default function Field({lable, value, onChange}){
     return(
         <div className="flex flex-col gap-1">
             <label htmlFor="" className="text-gray-500">{lable}</label>
-             <input type="text"  className="bg-[#FFFDFD] w-full border rounded-md py-3 px-3 outline-0 focus:ring-1 focus:ring-green-200 border-[#C3C3C3]" value={value}/>
+             <input type="text" onChange={onChange}  className="bg-[#FFFDFD] w-full border rounded-md py-3 px-3 outline-0 focus:ring-1 focus:ring-green-200 border-[#C3C3C3]" value={value}/>
         </div>
     )
 }
