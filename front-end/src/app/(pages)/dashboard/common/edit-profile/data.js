316008bfa4cbfb3540f83a1324bffb172045f950
Yasirhassan1 <yasuhassan7896@gmail.com>
2025-12-15 22:50:13 +0500

    feat: Implement initial edit profile page with a reusable field component and mock data.

---
+++ front-end/src/app/(pages)/dashboard/common/edit-profile/data.js
@@ -0,0 +1,21 @@
+import {v4 as uuidv4} from 'uuid';
+export const data =  
+    {
+        id:uuidv4(),
+        name: "Yasir Hassan",
+        applicationNo: "204453",
+        registrationNo: "F23BSCS044",
+        program: "BSCS",
+        department: "Computer Science",
+        session: "2023-2027",
+        gender: "Male",
+        dateOfBirth: "2005-01-01",
+        village: "Tolti",
+        permanentAddress: "P.O Tolti kharmang",
+        cellNo: "03554229637",
+        email: "yasirhassan@gmail.com",
+        fatherName: "Ghulam Hassan",
+        fatherCellNo: "03554229637",
+        fatherOccupation: "Retired",
+        
+    }
--- front-end/src/app/(pages)/dashboard/common/edit-profile/page.jsx
+++ front-end/src/app/(pages)/dashboard/common/edit-profile/page.jsx
@@ -1,5 +1,32 @@
+import Field from "@/app/components/profile/Field";
+import {data} from "./data";
 export default function EditProfile(){
     return(
-        <h1>Edit Profile</h1>
+        <div className="container flex flex-col gap-4">
+        <h3 className="">Edit Profile</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-12">
+       <Field lable="Name" value={data.name}/>
+       <Field lable="Application No" value={data.applicationNo}/>
+       <Field lable="Registration No" value={data.registrationNo}/>
+       <Field lable="Program" value={data.program}/>
+       <Field lable="Department" value={data.department}/>
+       <Field lable="Session" value={data.session}/>
+       <Field lable="Gender" value={data.gender}/>
+       <Field lable="Date of Birth" value={data.dateOfBirth}/>
+       <Field lable="Village" value={data.village}/>
+       <Field lable="Permanent Address" value={data.permanentAddress}/>
+       <Field lable="Cell No" value={data.cellNo}/>
+       <Field lable="Email" value={data.email}/>
+       <Field lable="Father Name" value={data.fatherName}/>
+       <Field lable="Father Cell No" value={data.fatherCellNo}/>
+       <Field lable="Father Occupation" value={data.fatherOccupation}/>
+        </div>
+
+        <div className="buttons flex flex-wrap gap-4 mt-12">
+            <button className="bg-green-700 px-4 py-2 rounded-lg text-white">Edit</button>
+            <button className="bg-green-700 px-4 py-2 rounded-lg text-white">Save</button>
+        </div>
+        </div>
+
     )
 }
